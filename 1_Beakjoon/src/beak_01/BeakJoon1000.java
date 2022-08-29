556635fe4060f608953eea241da242626c282d5f
19_web_hyr <19_web_hyr@302-15>
2022-08-29 20:16:57 +0900

    1_beak

---
+++ 1_Beakjoon/src/beak_01/BeakJoon1000.java
@@ -0,0 +1,18 @@
+package beak_01;
+
+import java.util.Scanner;
+
+public class BeakJoon1000 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("A 와 B 입력 :");
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		
+	}
+
+}
